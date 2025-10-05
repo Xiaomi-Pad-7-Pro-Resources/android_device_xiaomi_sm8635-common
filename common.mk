10d735dd2e57a9a572d03c74d2729b4eb253ebe9
dodyirawan85 <dev.irawans@gmail.com>
2025-10-05 21:21:20 +0700

    sm8635-common: Drop secure element support
    * Dependency of nfc, which we already drop previously
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- common.mk
+++ common.mk
@@ -236,11 +236,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# SecureElement
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
-    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
-
 # Security
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.xml
