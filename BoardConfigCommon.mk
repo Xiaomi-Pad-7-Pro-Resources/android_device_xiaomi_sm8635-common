6a218d5ec31264b28635668f0d907b5c22346fe5
dodyirawan85 <dev.irawans@gmail.com>
2025-10-05 22:37:47 +0700

    sm8635-common: Import properties from muyu OS2.0.200.0.VOYMIXM
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,6 +133,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # VINTF
