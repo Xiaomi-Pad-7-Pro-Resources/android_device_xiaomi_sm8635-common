8247328a7e5cad902125daecb9fb4680609b6245
dodyirawan85 <dev.irawans@gmail.com>
2025-12-19 13:55:57 +0700

    sm8635-common: Stop inherit common partition size
    * We already ship with gapps
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,9 +113,6 @@ $(foreach p, $(call to-upper, $(BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST))
     $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
-BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE := false
-include vendor/lineage/config/BoardConfigReservedSize.mk
-
 # Platform
 TARGET_BOARD_PLATFORM := pineapple
 TARGET_BOOTLOADER_BOARD_NAME ?= pineapple
